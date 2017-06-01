ececea1f9c1bbfbbca39c0352f12b06b06be7fa1
zhoujinl <zhoujinl@126.com>
2017-06-01 16:53:05 +0800

    step1 完善功能

--- autoDiscoveryProcess.sh
+++ autoDiscoveryProcess.sh
@@ -1,12 +1,19 @@
 # some final variables
 g_allpidfile=".getChoiceProcessPid.txt";
-g_destclassname="core.ApplicationProcess";
+g_tempJslonfile=".tempJsonFile.txt";
+g_applicationProcsss="core.ApplicationProcess";
 g_oneDateSeconds=86400;
+g_processMessageJsonPre="{\"$g_applicationProcsss\":[";
+g_processMessageJsonSuff="],";
+g_processRelationJsonPre="\"RelationShip\": [";
+g_processRelationJsonSuff="]}";
 
 # 前台传入
-g_keepRunningDates=72;
-g_neid=1000001011 ;
-g_sourclassname=1000001011 ;
+g_keepRunningDates=36;
+g_neid=1000001011;
+g_agentid=80008001;
+g_name=1000001011;
+g_sourclassname="core.Linux.Mainframe";
 g_user="logstash";
 g_processname="logstash";
 g_exclude="other"
@@ -15,39 +22,89 @@ g_exclude="other"
 #get all process
 function getChoiceProcess(){
 	if [ -n $g_user ] ; then
-		cmd="ps -f -u $g_user  ";
+		vcmd="ps -f -u $g_user  ";
 	else 
-		cmd="ps -ef  "
+		vcmd="ps -ef  "
 	fi;
 	if [ -n $g_processname ]; then	
-		cmd=$cmd" | grep $g_processname ";
+		vcmd=$vcmd" | grep $g_processname ";
 	fi;
 	if [ -n $g_exclude ]; then
-		cmd=$cmd" | grep -v  $g_exclude ";
+		vcmd=$vcmd" | grep -v  $g_exclude ";
 	fi;
 
-	eval $cmd | awk '{print $2}' > $g_allpidfile
-	cat .getChoiceProcessPid.txt
+	eval $vcmd | awk '{print $2}' > $g_allpidfile
+	#cat .getChoiceProcessPid.txt
 }
 
 function getProcessName(){
-	v_pid=$1;
-	v_processcmd=`ps -p $v_pid|grep -v "CMD"|awk '{print $4}'`;
-	v_processuser=`ps -p $v_pid -o user|grep -v "USER"`;
-	v_processname=$g_neid-$v_processuser-$v_processcmd;
-	echo $v_processname
+	vpid=$1;
+	vprocesscmd=`ps -p $vpid|grep -v "CMD"|awk '{print $4}'`;
+	vprocessuser=`ps -p $vpid -o user|grep -v "USER"`;
+	vprocessname=${g_neid}-${vpid}-${vprocessuser}-${vprocesscmd};
+	echo $vprocessname
 }
 function getProcessShortDiscription(){
-	v_pid=$1;
-	v_processcmd=`ps -p $v_pid|grep -v "CMD"|awk '{print $4}'`;
-	v_processuser=`ps -p $v_pid -o user|grep -v "USER"`;
-	v_processname=$v_processcmd;
-	echo $v_processname
+	vpid=$1;
+	vprocesscmd=`ps -p $vpid|grep -v "CMD"|awk '{print $4}'`;
+	vprocessname=$vprocesscmd;
+	echo $vprocessname
+}
+function getProcessUser(){
+	vpid=$1;
+	vprocessuser=`ps -p $vpid -o user|grep -v "USER"`;
+	echo $vprocessuser
 }
 
-function generateProcessJson(){
+## $1 pid
+function generateProcessMessage(){
+	vpid=$1;
+	vproName=$(getProcessName $vpid);
+	vproShorD=$(getProcessShortDiscription $vpid);
+	vproUser=$(getProcessUser $vpid);
 
+	vprcmd=$vproShorD;
+	#base64 加密
+	vproShorD=`echo "$vproShorD"|base64 `;
 	
+	vpre="{\"action\":\"10\",\"value\":{";
+	vsuff="}},";
+	vdatasetid="\"dataset_id\": \"7\",";
+	vname="\"name\":\""$vproName"\","
+	vshortdis="\"short_description\":\""$vproShorD"\","
+	vuser="\"process_user\":\""$vproUser"\","
+	vcmd="\"process_name\":\""$vprcmd"\","
+	vnameformat="\"name_format\": \"neId-pid-user-ProcessName\","
+
+	vmessage=${vdatasetid}${vnameformat}${vname}${vshortdis}${vuser}${vcmd}
+	vresult=${vpre}${vmessage}${vsuff} ;
+	echo $vresult;
+}
+
+## $1 pid
+function generateProcessRelation(){
+	vpid=$1;
+	vproName=$(getProcessName $vpid);
+	
+		
+	vpre="{\"action\":\"10\",\"value\":{";
+	vsuff="}},";
+	
+	vsourcepre="\"source\": {";
+	vsourcesuff="},";
+	vdistpre="\"destination\": {";
+	vdistsuff="}";
+	
+	vsourcename="\"name\":\""$g_name"\","
+	vsourceclass="\"class\":\""$g_sourclassname"\""
+	
+	vdistname="\"name\":\""$vproName"\","
+	vdistclass="\"class\":\""$g_applicationProcsss"\""
+
+	vsourceResult=${vsourcepre}${vsourcename}${vsourceclass}${vsourcesuff};
+	vdistResult=${vdistpre}${vdistname}${vdistclass}${vdistsuff};
+	vresult=${vpre}${vsourceResult}${vdistResult}${vsuff};
+	echo $vresult;
 }
 
 # get the process running seconds
@@ -88,23 +145,53 @@ function getProcessRunningOverDays(){
 	if [ ! -f $g_allpidfile ] ;then
 		return;
 	fi;
+	
+	vmessagebuild=$g_processMessageJsonPre;
+	vrelationbuild=$g_processRelationJsonPre;
+	
 	for pid in `cat $g_allpidfile` 
 	do
 		isfound=$(isProcessRunningOverSpecifyDays $pid)
+		#TODO isListen 	
 		if [ $isfound -eq 1 ] ; then 
-			echo  $pid $isfound;
-			
-			
+			##执行操作
+			x=$(generateProcessMessage $pid);
+			y=$(generateProcessRelation $pid);
+			vmessagebuild=${vmessagebuild}$x;
+			vrelationbuild=${vrelationbuild}$y;
 		fi;
 	done;
+	vmessagebuild=${vmessagebuild}${g_processMessageJsonSuff}
+	vrelationbuild=${vrelationbuild}${g_processRelationJsonSuff};
+	echo ${vmessagebuild}${vrelationbuild} > $g_tempJslonfile
 }
 
+function getJsonFileName(){
+	vfilename="CFG_$(g_neid)_7_$(g_agentid)";
+}
+function getFinallyResult(){
 
+	if [ ! -f $g_tempJslonfile ];then 
+		echo "xxx"
+		exit -1;
+	fi
+		
+}
 
-printf "Main the process has run for %s seconds\n" "$age"
-x=$( getChoiceProcess )
+printf "=============================>> begin.\n"
+##1.查找所有的进程
+getChoiceProcess
+##2.过滤出，符合条件的进程,并进行封装，输出
 getProcessRunningOverDays
-generateProcessJson 4446
+##3.生成最终的CFG文件
+getFinallyResult
+
+printf "=============================>> end.\n"
+
+
+
+
+
 
 
 
